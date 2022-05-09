6972fade6034ec443efd81a7cc39e8bf6307f2a7
sahilwahi <sahil.wahi@phenompeople.com>
2022-05-09 13:35:32 +0530

    commit5

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-27&g&y58o-q&#&xxko7%quwq3gf!hir@o@q*^5%0j1sc+(c_88
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sahilwahiportfolio.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','sahilwahi.herokuapp.com']
 
 
 # Application definition
